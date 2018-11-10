d349e572bb07c1bc424a4ff59a07a666b0be7877
xaster <jifengzhuiying@sina.com>
2018-11-10 15:42:13 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # docker-nginx-debian
+Docker: Nginx With All Official Modules And Some Third Party Modules (Based On Debian)
+
+# Third Party Modules
+```
+Jemalloc
+Redis
+OpenSSL
+Nginx Module:Cache-Purge
+Nginx Module:Brotli
+Nginx Module:PageSpeed
+Nginx Module:Redis
+Nginx Module:Devel-Kit
+Nginx Module:Set-Misc
+Nginx Module:Echo
+Nginx Module:Redis2
+Nginx Module:Srcache
+```
+
+# Usage
+```
+$ docker pull xaster/docker-nginx-debian
+
+$ docker run -d \
+    --name docker-nginx-debian \
+    -v /NGINX_WEB_DIR:/usr/share/nginx/html \
+    -v /NGINX_CONFIG_DIR:/etc/nginx \
+    -v /NGINX_CERTS_DIR:/etc/certs \
+    -v /NGINX_LOG_DIR:/var/log/nginx \
+    -v /NGINX_CACHE_DIR:/var/cache/nginx \
+    -v /NGINX_PID_DIR:/var/run/nginx \
+    -v /REDIS_CONFIG_DIR:/etc/redis \
+    -v /REDIS_LOG_DIR:/var/log/redis \
+    -v /REDIS_DATA_DIR:/var/lib/redis \
+    -v /REDIS_PID_DIR:DATA/var/run/redis \
+    -p 80:80 -p 443:443 \
+    -e TIMEZONE=YOUR_TIME_ZONE \
+    xaster/docker-nginx-debian
+```
---
+++ Run.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+test -f '/etc/redis/redis.conf' || cp -a /etc/redis_default/redis.conf /etc/redis/redis.conf
+test -f '/etc/redis/redis.conf.default' || cp -a /etc/redis_default/redis.conf /etc/redis/redis.conf.default
+test -f '/etc/nginx/fastcgi.conf' || cp /etc/nginx_default/fastcgi.conf /etc/nginx/fastcgi.conf
+test -f '/etc/nginx/fastcgi.conf.default' || cp /etc/nginx_default/fastcgi.conf /etc/nginx/fastcgi.conf.default
+test -f '/etc/nginx/fastcgi_params' || cp /etc/nginx_default/fastcgi_params /etc/nginx/fastcgi_params
+test -f '/etc/nginx/fastcgi_params.default' || cp /etc/nginx_default/fastcgi_params /etc/nginx/fastcgi_params.default
+test -f '/etc/nginx/mime.types' || cp /etc/nginx_default/mime.types /etc/nginx/mime.types
+test -f '/etc/nginx/mime.types.default' || cp /etc/nginx_default/mime.types /etc/nginx/mime.types.default
+test -f '/etc/nginx/nginx.conf' || cp /etc/nginx_default/nginx.conf /etc/nginx/nginx.conf
+test -f '/etc/nginx/nginx.conf.default' || cp /etc/nginx_default/nginx.conf /etc/nginx/nginx.conf.default
+test -f '/etc/nginx/scgi_params' || cp /etc/nginx_default/scgi_params /etc/nginx/scgi_params
+test -f '/etc/nginx/scgi_params.default' || cp /etc/nginx_default/scgi_params /etc/nginx/scgi_params.default
+test -f '/etc/nginx/uwsgi_params' || cp /etc/nginx_default/uwsgi_params /etc/nginx/uwsgi_params
+test -f '/etc/nginx/uwsgi_params.default' || cp /etc/nginx_default/uwsgi_params /etc/nginx/uwsgi_params.default
+test -f '/etc/nginx/win-utf' || cp /etc/nginx_default/win-utf /etc/nginx/win-utf
+test -f '/etc/nginx/koi-utf' || cp /etc/nginx_default/koi-utf /etc/nginx/koi-utf
+test -f '/etc/nginx/koi-win' || cp /etc/nginx_default/koi-win /etc/nginx/koi-win
+test -d '/etc/nginx/conf.d' || mkdir /etc/nginx/conf.d
+test -f '/etc/nginx/conf.d/default.conf.default' || cp /etc/nginx_default/conf.d/default.conf /etc/nginx/conf.d/default.conf.default
+if [ $TIMEZONE ]; then
+  rm -rf /etc/localtime
+  echo "${TIMEZONE}" > /etc/timezone
+  cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+fi
+find /etc/nginx/conf.d -name "*.conf" | grep -q ".conf"
+if [ $? -ne 0 ];then
+  cp /etc/nginx_default/conf.d/default.conf /etc/nginx/conf.d/default.conf
+fi
+find /usr/share/nginx/html -name "index.*" | grep -q "index."
+if [ $? -ne 0 ];then
+  cp /usr/share/nginx/html_default/index.html /usr/share/nginx/html/index.html
+  cp /usr/share/nginx/html_default/50x.html /usr/share/nginx/html/50x.html
+fi
+/usr/bin/redis-server /etc/redis/redis.conf
+/usr/sbin/nginx -g 'daemon off;'
